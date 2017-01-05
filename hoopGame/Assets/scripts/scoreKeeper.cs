a7ddcbe052e3c54e63e3e0dd6d305bc610fe08b4
KMSkelton <kmskelton@outlook.com>
2017-01-04 23:20:12 -0800

    cube target added. scoring added.

---
+++ hoopGame/Assets/scripts/scoreKeeper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scoreKeeper : MonoBehaviour {
+	public int score = 0;
+
+	void OnCollisionEnter(Collision collision) {
+		print ("Ball hit target");
+		score += 1;
+		print("Score = " + score);
+	}
+}
