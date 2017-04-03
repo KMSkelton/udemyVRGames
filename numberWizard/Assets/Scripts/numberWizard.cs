1811efa01b689d07985a24cae1dc6165fc23e621
KMSkelton <kmskelton@outlook.com>
2017-04-02 22:49:26 -0700

    adding numberWizard text guessing game

---
+++ numberWizard/Assets/Scripts/numberWizard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class numberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Welcome to Number Wizard \n Pick a number, but don't tell me.");
+
+		int max = 1000;
+		int min = 1;
+
+		print("Please select a number between " + min + " and " + max + ".");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
